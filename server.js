7f8035d39fb8491a8029adc637d2e6021b3cbe76
emiliorz2 <emiliorz2@icloud.com>
2024-07-13 13:07:07 -0600

    library unique ids, and trying to show notes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
+
 const { log } = require('console');
 const express =  require('express');
 const path = require('path');
@@ -8,6 +10,7 @@ const PORT = 3000;
 const PUBLIC = path.join(__dirname, 'public');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(PUBLIC));
 
 let notas = []
@@ -18,35 +21,49 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(PUBLIC, 'home.html'));
 });
 
+
+//agregar
 app.post('/agregarNota', (req, res) => {
     console.log('agregando nota');
     console.log(req.body)
 
     console.log('\ntodas las notas');
-    notas.push({id: 1 , ...req.body, date: new Date()})
+    notas.push({id: uuidv4() , ...req.body, date: new Date()})
     console.log(notas);
     res.redirect('/');
 });
 
-// app.get('/contactus', (req,res) => {
-//     console.log('loading contact us.....');
-//     res.sendFile(path.join(PUBLIC, 'contactus.html'));
-// })
+//actualizar
+app.get('/notas/:id', (req, res) => {
+    const nota = notas.find(n => n.id === req.params.id);
+    if (nota) {
+        res.json(nota);
+    } else {
+        res.status(404).send('Nota no encontrada');
+    }
+});
 
-// app.post('/contactus', (req,res) => {
-//     console.info('Contact us has been called...');
-//     const name = req.body.name;
-//     const email = req.body.email;
-//     const subject = req.body.subject;
+app.put('/notas/:id', (req, res) => {
+    const index = notas.findIndex(n => n.id === req.params.id);
+    if (index !== -1) {
+        notas[index] = { ...notas[index], ...req.body, date: new Date() };
+        res.send('Nota actualizada');
+    } else {
+        res.status(404).send('Nota no encontrada');
+    }
+});
 
-//     console.log('Form data:\n');
-//     console.log('Name: ' + name);
-//     console.log('Email: ' + email);
-//     console.log('Subject: ' + subject);
+//eliminar
+app.delete('/notas/:id', (req, res) => {
+    const index = notas.findIndex(n => n.id === req.params.id);
+    if (index !== -1) {
+        notas.splice(index, 1);
+        res.send('Nota eliminada');
+    } else {
+        res.status(404).send('Nota no encontrada');
+    }
+});
 
-//     res.redirect('/');
-
-// })
 
 app.listen(PORT, () => {
     console.log("☆*: .｡. o(≧▽≦)o .｡.:*☆ el server esta corriendo en el puerto: " + PORT)
